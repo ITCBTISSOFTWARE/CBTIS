cc1c47b83375fc65745952a76b3a3f1541347f11
ITCBTISSOFTWARE <61855362+ITCBTISSOFTWARE@users.noreply.github.com>
2020-03-05 21:58:21 -0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Mi primera pagina web</title>
+  </head>
+<center>
+<body background="img/WhatsApp Image 2020-03-04 at 8.01.22 PM.jpeg" width=680px heigh=680px > </center>
+<table width="100">
+	<tr>
+		<td width="100%"><img src="img/cbtis.png"></td>
+	</tr>
+</table>
+</body>
+</html>
